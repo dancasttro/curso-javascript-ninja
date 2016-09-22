946405707fb3c9c4af8f898e1fb1053957351357
Daniel Castro <dancasttro@gmail.com>
2016-09-22 17:25:11 -0300

    finalizando challenge-09

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -98,7 +98,10 @@ myFunction3();
   `division` e `mod`, e atribua à elas `calculator`, passando números
   diferentes para cada chamada.
   */
-  // ?
+  var subtraction = calculator( 5, 2 );
+  var multiplication = calculator( 10, 6 );
+  var division = calculator( 100, 12  );
+  var mod = calculator( 40, 15 );
 
   /*
   Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -108,15 +111,24 @@ myFunction3();
   chamada.
   */
   console.log( 'O resultado da subtração é:' );
-  // ?
+  console.log( subtraction( function( number1, number2  ){
+    return number1 - number2;
+  })  );
 
   console.log( 'O resultado da multiplicação é:' );
-  // ?
+  console.log( multiplication( function( number1, number2  ){
+    return number1 * number2;
+  })  );
 
   console.log( 'O resultado da divisão é:' );
-  // ?
+  console.log( division( function( number1, number2  ){
+    return number1 / number2;
+  })  );
+
 
   console.log( 'O resto da divisão é:' );
-  // ?
+  console.log( mod( function( number1, number2  ){
+    return number1 % number2;
+  })  );
 
 })();
